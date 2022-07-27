58cc2311dcecd15eb09cd817c9246a0c92d7d878
jhoan-molina <jhoansebastianmolinasierra@gmail.com>
2022-07-27 17:02:53 -0500

    Reestablecer

---
+++ app/src/main/java/com/example/parcbarcos/reestablecer_pass.java
@@ -0,0 +1,14 @@
+package com.example.parcbarcos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reestablecer_pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reestablecer_pass);
+    }
+}
