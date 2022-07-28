e1b53a06498613d6d0ecc1e185bda7565c57e4df
jhoan-molina <jhoansebastianmolinasierra@gmail.com>
2022-07-28 00:06:42 -0500

    Crear Date

--- app/src/main/java/com/example/parcbarcos/admin_viajes_crear.java
+++ app/src/main/java/com/example/parcbarcos/admin_viajes_crear.java
@@ -3,7 +3,10 @@ package com.example.parcbarcos;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,33 +22,45 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class admin_viajes_crear extends AppCompatActivity {
-    EditText editId_admin_isert_viajes, editDestino_admin_isert_viajes,editId_barco_admin_insert_viajes,
+    EditText editId_admin_isert_viajes, editFS_admin_insert_viajes, editFL_admin_insert_viajes,editDestino_admin_isert_viajes,editId_barco_admin_insert_viajes,
             edit_Id_patron_admin_insert_viajes;
 
-    DatePicker editFS_admin_insert_viajes, editFL_admin_insert_viajes;
-
+    /*DatePicker editFS_admin_insert_viajes;, editFL_admin_insert_viajes;*/
+    DatePickerDialog.OnDateSetListener onDateSetListener, onDateSetListener1;
 
     Button btnAgregrar_admin_insert_viajes;
     RequestQueue requestQueue;
+
+    final Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH);
+    int day = calendar.get(Calendar.DAY_OF_MONTH);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_viajes_crear);
 
+
+
         editId_admin_isert_viajes= findViewById(R.id.editId_admin_insert_viajes);
-        //editFS_admin_insert_viajes = findViewById(R.id.editFS_admin_insert_viajes);
-        //editFL_admin_insert_viajes= findViewById(R.id.editFL_admin_insert_viajes);
+        editFS_admin_insert_viajes = findViewById(R.id.editFS_admin_insert_viajes);
+        editFL_admin_insert_viajes= findViewById(R.id.editFL_admin_insert_viajes);
         editDestino_admin_isert_viajes = findViewById(R.id.editDestino_admin_insert_viajes);
         editId_barco_admin_insert_viajes= findViewById(R.id.editId_barco_admin_insert_viajes);
         edit_Id_patron_admin_insert_viajes = findViewById(R.id.edit_Id_patron_admin_insert_viajes);
 
         btnAgregrar_admin_insert_viajes=findViewById(R.id.btnAgregrar_admin_insert_viajes);
         btnAgregrar_admin_insert_viajes.setOnClickListener(this::onClick);
+        editFS_admin_insert_viajes.setOnClickListener(this::onClick);
+        editFL_admin_insert_viajes.setOnClickListener(this::onClick);
+
     }
 
     private void insertarViajeAdmin(String url) {
@@ -87,7 +102,29 @@ public class admin_viajes_crear extends AppCompatActivity {
         int id= view.getId();
         if(id==R.id.btnAgregrar_admin_insert_viajes){
             insertarViajeAdmin("http://192.168.0.12/crud_club_barcos/admin/viajes/insert.php");
+        }else if(id==R.id.editFS_admin_insert_viajes){
+            DatePickerDialog datePickerDialog = new DatePickerDialog(admin_viajes_crear.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    month = month+1;
+                    String date = day+"/"+month+"/"+year;
+                    editFS_admin_insert_viajes.setText(date);
+                }
+            },year, month, day);
+            datePickerDialog.show();
+
+        } else if(id==R.id.editFL_admin_insert_viajes) {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(admin_viajes_crear.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    month = month+1;
+                    String date = day+"/"+month+"/"+year;
+                    editFL_admin_insert_viajes.setText(date);
+                }
+            },year, month, day);
+            datePickerDialog.show();
+            }
         }
 
-    }
+
 }
